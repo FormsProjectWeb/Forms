2b69d56daf6d5a4276a21533ea69bf0c576e3013
wareval0 <w.arevalo@uniandes.edu.co>
2024-04-18 12:59:02 -0500

    New Project

--- Deploy/deployment.sh
+++ Deploy/deployment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_ID="master-vehicle-420613"
+PROJECT_ID="singular-cache-415315"
 ZONE="us-central1-a"
 
 gcloud deployment-manager deployments create form-deployment --config Deploy/deployment.yaml --project $PROJECT_ID
